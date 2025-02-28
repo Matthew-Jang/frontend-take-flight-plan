5b2c963fb30b09fc0d4fca5c59dda440b10a82f7
mrj <michael.jang@eagles.oc.edu>
2025-02-28 13:56:33 -0600

    majors

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Login from "./views/Login.vue";
 import Home from "./views/Home.vue";
 
 import BadgeList from '@/components/BadgeList.vue';
+import MajorList from '@/components/MajorList.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,6 +26,12 @@ const router = createRouter({
       name: 'AdminBadges',
       component: BadgeList, 
       meta: { requiresAdmin: true}
+    },
+    {
+      path: '/admin/majors',
+      name: 'AdminMajors',
+      component: MajorList,
+      meta: { requiresAdmin: true } //Matt, Does this work?
     }
   ],
 });
