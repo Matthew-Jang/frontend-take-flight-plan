5acca4198f1e3248e246d7027225c5818349b0f2
mjangoc <michael.jang@eagles.oc.edu>
2025-03-25 16:49:03 -0500

    Update UserProfile.vue

--- UserProfile.vue
+++ UserProfile.vue
@@ -29,9 +29,9 @@ export default {
         points: 1500,
         avatar: "",
         badges: [
-          { name: "Early Bird", imageUrl: require('@/assets/images/badge0.png') },
-          { name: "The Worm", imageUrl: require('@/assets/images/badge1.png') },
-          { name: "OC Eagle", imageUrl: require('@/assets/images/badge2.png') }
+          { name: "Early Bird", imageUrl: require('@/assets/images/badges/badge0.png') },
+          { name: "The Worm", imageUrl: require('@/assets/images/badges/badge1.png') },
+          { name: "OC Eagle", imageUrl: require('@/assets/images/badges/badge2.png') }
         ]
       }
     };
