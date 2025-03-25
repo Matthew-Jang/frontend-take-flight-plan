76e8711e85d1bb6a576214b78fc6f673a1827b1d
mjangoc <michael.jang@eagles.oc.edu>
2025-03-25 16:15:58 -0500

    Update UserProfile.vue

--- UserProfile.vue
+++ UserProfile.vue
@@ -1,1 +1,21 @@
+<template>
+  <div class="profile-card">
+    <!-- Avatar Image -->
+    <img :src="user.avatar" alt="User Avatar" class="avatar" />
 
+    <!-- Upload Profile Picture -->
+    <input type="file" @change="handleFileUpload" accept="image/*" />
+
+    <!-- User Details -->
+    <h1>{{ user.name }}</h1>
+    <p>Worm Points: {{ user.points }}</p>
+    
+    <!-- Display Badges as Images with Text Underneath -->
+    <div class="badges">
+      <div v-for="badge in user.badges" :key="badge.name" class="badge">
+        <img :src="badge.imageUrl" alt="badge image" class="badge-img" />
+        <p>{{ badge.name }}</p>
+      </div>
+    </div>
+  </div>
+</template>
