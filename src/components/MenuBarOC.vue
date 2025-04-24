a7191cac484bd07f22172c0ad4d19696b4270b20
matthew-jang <matthew.jang@eagles.oc.edu>
2025-04-24 10:21:53 -0500

    menubar initials

--- src/components/MenuBarOC.vue
+++ src/components/MenuBarOC.vue
@@ -16,7 +16,7 @@ const resetMenu = () => {
   user.value = null;
   user.value = Utils.getStore("user");
   if (user.value) {
-    initials.value = user.value.fName[0] + user.value.lName[0];
+    // initials.value = user.value.fName[0] + user.value.lName[0];
     name.value = user.value.fName + " " + user.value.lName;
   }
 }
